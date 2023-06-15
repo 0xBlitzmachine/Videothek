9989830553c937b2d9a8e87968e6c82806dbca28
Blitzmachine <blitzm4ch1n3@gmail.com>
2023-06-15 12:23:47 +0200

    Removed "force no-null" since the method got an improvement

--- src/main/kotlin/MovieHandling.kt
+++ src/main/kotlin/MovieHandling.kt
@@ -3,14 +3,14 @@ val movieCollection = mutableMapOf<String, Movie>()
 fun addMovie(key: String, movieClass: Movie): String {
     if (!movieCollection.containsKey(key))
         movieCollection[key] = movieClass
-    return Message().getMessageByKey("ADD_MOVIE_SUCCESS")!!.format(key, movieClass.genre, movieClass.price)
+    return Message().getMessageByKey("ADD_MOVIE_SUCCESS").format(key, movieClass.genre, movieClass.price)
 }
 
 fun removeMovie(key: String): String {
     if (!movieCollection.containsKey(key))
-        return Message().getMessageByKey("REMOVE_MOVIE_DOES_NOT_EXIST")!!.format(key)
+        return Message().getMessageByKey("REMOVE_MOVIE_DOES_NOT_EXIST").format(key)
     else {
         movieCollection.remove(key)
-        return Message().getMessageByKey("REMOVE_MOVIE_SUCCESS")!!.format(key)
+        return Message().getMessageByKey("REMOVE_MOVIE_SUCCESS").format(key)
     }
 }
