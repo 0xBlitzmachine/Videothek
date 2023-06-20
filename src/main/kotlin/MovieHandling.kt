71b3324052d30c165241ace8af02af5882ec1d97
Blitzmachine <blitzm4ch1n3@gmail.com>
2023-06-20 12:56:03 +0200

    Changing behaviour of functions

--- src/main/kotlin/MovieHandling.kt
+++ src/main/kotlin/MovieHandling.kt
@@ -1,30 +1,14 @@
-val movieCollection = mutableMapOf<String, Movie>()
+class Cinema {
+    private var movieCollection: MutableMap<Int, Movie> = mutableMapOf()
+    private var id = movieCollection.keys.max()
 
-fun addMovie(key: String, movieClass: Movie): String {
-    if (movieCollection.containsKey(key))
-        return Message.ADD_MOVIE_KEY_EXISTS.value.format(key)
+    fun addMovie(movie: Movie) {
+        if (id == 0) {
+            id = 1
+            movieCollection[id] = movie
+            return
+        }
 
-        movieCollection[key] = movieClass
-    return Message.ADD_MOVIE_SUCCESS.value.format(key, movieClass.genre, movieClass.price!!)
-}
-
-fun removeMovie(key: String): String {
-    if (!movieCollection.containsKey(key))
-        return Message.REMOVE_MOVIE_DOES_NOT_EXIST.value.format(key)
-
-        movieCollection.remove(key)
-        return Message.REMOVE_MOVIE_SUCCESS.value.format(key)
-}
-
-fun showAllMovies() {
-    movieCollection.forEach {
-        println("PLACEHOLDER")
+        movieCollection[]
     }
-}
-
-fun addDummies(listofmovies: List<String>): String {
-    listofmovies.forEach {
-        addMovie(it, Movie(it, null, randomPrice(), generateRatings()))
-    }
-    return "Dummy Collection of movies has been added!"
 }
